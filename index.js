3e1d953fce1410dc99385c4be55ff9a59169298c
VictorM <neuron96@mail.ru>
2021-11-17 12:50:11 +0400

    made changes in master1

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 console.log('hello world')
 
-const age = 20;
-
-if (age >= 18) {
-    console.log('Вы совершеннолетний');
+const userAge = 35;
+if (userAge >= 18) {
+    console.log('Вы совершеннолетний')    
 } else {
-    console.log('Вы НЕ совершеннолетний');
+    console.log('Вы НЕ совершеннолетний')
 }
